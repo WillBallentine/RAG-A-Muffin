9d07a5ea24d54f916ada15f8b3a2c02b7f0aca5c
Will Ballentine <willballentine@Wills-MacBook-Pro.local>
2026-05-11 22:09:16 -0400

    initial commit

---
+++ rag-a-muffin/Models/uploadrequest.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace RagAMuffin.Models
+{
+    public class UploadRequest
+    {
+        public string Name { get; set; }
+    }
+}
